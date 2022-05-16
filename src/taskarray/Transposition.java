53ee5576013e40d836af002a9a377271e2eee0c2
zenit1925 <kostia-85-85@yandex.ru>
2022-05-16 17:14:53 +0300

    1.8. Транспонировать матрицу

---
+++ src/taskarray/Transposition.java
@@ -0,0 +1,20 @@
+package taskarray;
+
+import java.util.Arrays;
+
+/**
+ * 1.8. Транспонировать матрицу
+ * Транспонирование матрицы, это когда строки исходной матрицы, становятся столбцами в результирующей матрице
+ */
+public class Transposition {
+    public static int[][] convert(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] result = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                result[j][i] = matrix[i][j];
+        }
+        return result;
+    }
+}
