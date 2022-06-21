ef440ae0ca6676cbb704845aafc71d97740f08a4
zenit1925 <kostia-85-85@yandex.ru>
2022-06-21 14:28:16 +0300

    0.2. Размер списка и добавление элементов. ArrayList. List.

---
+++ src/List/AddElement.java
@@ -0,0 +1,12 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddElement {
+    public static boolean addNewElement(List<String> list, String str) {
+        List<String> check = new ArrayList<>(list);
+        check.add(0, str);
+        return true;
+    }
+}
