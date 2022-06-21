d914bd81e34bbc15e53e4969d60ddbbc6240dc18
zenit1925 <kostia-85-85@yandex.ru>
2022-06-21 16:05:35 +0300

    0.3. Добавление элемента в список по индексу

---
+++ src/List/AddIndexElement.java
@@ -0,0 +1,21 @@
+package List;
+/**
+ * Задание: необходимо реализовать метод, который добавляет элемент в список по индексу,
+ * при этом он должен добавлять элемент только в том случае, если этого элемента еще нет в коллекции.
+ * При этом метод должен вернуть булево значение. Для этого необходимо сравнить размер дубликата списка
+ * и списка в который мы добавляем элемент. Используйте методы contains(E e), add(int index, E e), size().
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddIndexElement {
+    public static boolean addNewElement(List<String> list, int index, String str) {
+        List<String> check = new ArrayList<>(list);
+        if (!list.contains(str)) {
+            list.add(index, str);
+            return list.size() > check.size();
+        }
+        return false;
+    }
+}
