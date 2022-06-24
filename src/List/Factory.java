56f710865cd8fbb2b4499cd6c4ca6d34eb0e1609
zenit1925 <kostia-85-85@yandex.ru>
2022-06-24 11:00:11 +0300

    0.5. Заполнение списка фабричным методом

--- src/List/Factory.java
+++ src/List/Factory.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class Factory {
     public static void main(String[] args) {
-        List<String> list = List.of("first", "two", "three", "fifth");
+        List<String> list = List.of("first", "second", "third", "fourth", "fifth");
         for (int i = 0; i < list.size(); i++) {  // здесь лист сайз это максимум размер листа
             System.out.println(list.get(i));     // здесь лист через метод гет выводит все эелементы списка
         }
