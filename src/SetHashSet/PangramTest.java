7a654160b679e1821c295b36a834d8fa9b076333
zenit1925 <kostia-85-85@yandex.ru>
2022-07-07 13:44:40 +0300

    0.3. Панграмма

---
+++ src/SetHashSet/PangramTest.java
@@ -0,0 +1,15 @@
+package SetHashSet;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PangramTest {
+    @Test
+    public void checkString() {
+        String str = "Jackdaws love my big sphinx of quartz";
+        boolean b = Pangram.checkString(str);
+        assertThat(b, is(true));
+    }
+}
